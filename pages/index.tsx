4bc2f00f4124df5ff7368282771681aec909f71c
UhRhythm <uhrhythm.0@gmail.com>
2025-05-13 07:56:25 +0900

    ボタンのテキストを「ほかのにゃんこもみるddd」から「One more cat!」に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        ほかのにゃんこもみるddd
+        One more cat!
       </button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
     </div>
